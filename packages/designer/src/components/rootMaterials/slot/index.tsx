ac888417e3db0f9b09bc3f01b0973167a33661e3
sunkeysun <sunkeysun@tencent.com>
2023-08-09 15:46:09 +0800

    feat: 插槽初始化优化

--- packages/designer/src/components/rootMaterials/slot/index.tsx
+++ packages/designer/src/components/rootMaterials/slot/index.tsx
@@ -1,10 +1,16 @@
-import { forwardRef } from 'react'
+import { forwardRef, Children } from 'react'
 
-export const Slot = forwardRef(({ children }, ref) => {
-  console.log(children, '123')
-  return (
-    <div ref={ref} style={{ width: 30, height: 30, backgroundColor: 'grey' }}>
-      {children}
-    </div>
-  )
-})
+export const Slot = forwardRef<HTMLDivElement>(
+  ({ children }: { children?: React.ReactNode }, ref) => {
+    if (Children.toArray(children)?.length > 0) return children
+
+    return (
+      <div
+        ref={ref}
+        style={{ minWidth: 20, minHeight: 20, backgroundColor: 'grey' }}
+      >
+        Slot
+      </div>
+    )
+  },
+)
